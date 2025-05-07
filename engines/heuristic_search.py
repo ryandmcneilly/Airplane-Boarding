af155b90a340696134625abe393ca93dfcb6d5b4
ryandmcneilly <ryanmcn@live.com.au>
2025-05-07 18:22:57 +1000

    Added best heuristic search

---
+++ engines/heuristic_search.py
@@ -0,0 +1,16 @@
+from engines.max_settle_row import MaxSettleRow
+from engines.outside_in_btf import OutsideInBTF
+from engines.random_ordering import RandomSolver
+from util import AirplaneBoardingProblem, Passenger
+
+
+def get_best_heuristic(abp: AirplaneBoardingProblem) -> tuple[int, list[Passenger]]:
+    makespan: float = float("inf")
+    ordering: list[Passenger] = ...
+    for Heuristic in [MaxSettleRow, OutsideInBTF, RandomSolver]:
+        solution = Heuristic().solve(abp)
+        heuristic_makespan = solution.simulate_seating()
+        if heuristic_makespan < makespan:
+            makespan, ordering = heuristic_makespan, solution.ordering
+
+    return int(makespan), ordering
