57d48586b1f7146ad312b9eef9cf257ff5f949f5
ryandmcneilly <ryanmcn@live.com.au>
2025-06-05 21:51:47 +1000

    Print table for solutions for easier view

--- sim.py
+++ sim.py
@@ -44,8 +44,31 @@ def run_solvers_on_abp(filepath: AbpFilepath):
             json.dump(result, f)
 
 
+def print_data_set_results(num_rows: int, num_cols: int):
+    folder_path = f"results/{num_rows}_{num_cols}"
+    print(f"{'file':38} {'lower_bound':>12} {'upper_bound':>12} {'gap (%)':>8} {'objective':>12} {'time (s)':>10}")
+
+    round_decimal = lambda val: f"{val:.2f}" if not (val in ["N/A", INVALID]) else INVALID
+    for filename in sorted(os.listdir(folder_path)):
+        if not filename.endswith(".json"):
+            continue
+        file_path = os.path.join(folder_path, filename)
+
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+            lb = round_decimal(data.get("lower_bound", INVALID))
+            ub = round_decimal(data.get("upper_bound", INVALID))
+            gap = round_decimal(data.get("gap", INVALID))
+            objective_value = round_decimal(data.get("objective_value", INVALID))
+            computation_time = round_decimal(data.get("computation_time", INVALID))
+
+            print(f"{filename:38} {lb:>12} {ub:>12} {gap:>8} {objective_value:>12} {computation_time:>10}")
+
 if __name__ == "__main__":
-    for num_rows, num_cols in [(20, 2)]:
+    # [(10, 4), (10, 6), (20, 4), (20, 6), (30, 2), (30, 4), (30, 6)]
+    for num_rows, num_cols in [(10, 6)]:
         for test_number in range(10):
             filepath = AbpFilepath(num_rows, num_cols, test_number)
             run_solvers_on_abp(filepath)
+    print_data_set_results(20, 2)
